377efe8b28deed2886ce7406785e56c9b06dfc9a
oxygen0605 <ozon0605@gmail.com>
2021-05-05 02:53:22 +0900

    feat: ODEソルバーにsolve_ivpを使用

--- bolie_model.py
+++ bolie_model.py
@@ -7,15 +7,74 @@ Created on Tue May  4 15:41:56 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.integrate import solve_ivp 
 
+
+def f(t, x,a,b,c,d):
+    i, g = x
+    p = q = 0
+    if 6 <= t < 7:
+        q = 100/17.5
+        #print(t)
+    dI_dt = p - a * i + b * g
+    dG_dt = q - c * i - d * g
+    return [dI_dt, dG_dt]
+
+
+def drow_figs(t, g, i):
+    
+    g_label= r"G(t) blood glucose level [mmol/L]"
+    i_label = r"I(t) blood insulin level [unit/L]"
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ln1 = ax1.plot(t, g, 'C0', label = g_label) 
+    
+    ax2 = ax1.twinx()
+    ln2 = plt.plot(t, i, 'C1',label = i_label)
+    
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1+h2, l1+l2, loc='upper right', fontsize=10)
+    
+    ax1.set_xlabel('time [hours]')
+    ax1.set_ylabel(g_label)
+    ax1.set_xlim([0,24])
+    #ax1.set_ylim([-10,60])
+    ax1.grid(True)
+    ax2.set_ylabel(i_label)
+    ax2.set_xlim([0,24])
+    #ax2.set_ylim([-0.01, 0.06])
+    
+    
+    plt.show()
+
+if __name__ == '__main__':
+    dt = 0.01
+    hr = 48
+    #t = np.linspace(0, hr, int(hr/dt+1))
+    t = np.arange(0, hr, dt)
+    
+    alpha = 0.916; beta = 0.198; gamma = 3.23; delta = 3.04
+    #alpha = 0.916; beta = 0.198; gamma = 3; delta = 3.04
+    d = (alpha+delta)**2 - 4*(alpha*delta + beta*gamma)
+    print("D: {}".format(d))
+
+    x0 = np.array([0.0, 0.0])
+    solver = solve_ivp (f, [0,48], x0, t_eval=t, args=(alpha, beta, gamma, delta),dense_output=True, max_step=dt)
+    
+    g = solver.y[1,:]*100 # g/l -> mg/dL
+    g /= 18 # mg/dL -> mmol/L
+    i = solver.y[0, :]
+    drow_figs(t, g, i)
+
+
+"""
 dt = 0.01
 hr = 48
 t = np.linspace(0, hr, int(hr/dt+1))
-
-alpha = 0.916
-beta =  0.198
-gamma = 3.23
-delta = 3.04
+alpha = 0.916; beta = 0.198; gamma = 3.23; delta = 3.04
 
 p = np.zeros((1, int(hr/dt+1)))
 q = np.zeros((1, int(hr/dt+1)))
@@ -37,23 +96,23 @@ x[1, :] = x[1, :] * 100
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-ln1 = ax1.plot(t, x[1, :], 'C0', label = "blood glucose level [mg/dL]") 
+ln1 = ax1.plot(t, x[1, :], 'C0', label = "G(t) blood glucose level [mg/dL]") 
 
 ax2 = ax1.twinx()
-ln2 = plt.plot(t, x[0, :], 'C1',label = "blood insulin level [unit/L]")
+ln2 = plt.plot(t, x[0, :], 'C1',label = "I(t) blood insulin level [unit/L]")
 
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
 ax1.legend(h1+h2, l1+l2, loc='upper right', fontsize=10)
 
 ax1.set_xlabel('time [hours]')
-ax1.set_ylabel(r'blood glucose level [mg/dL]')
+ax1.set_ylabel(r'G(t) blood glucose level [mg/dL]')
 ax1.set_xlim([0,24])
-ax1.set_ylim([-10,60])
+#ax1.set_ylim([-10,60])
 ax1.grid(True)
-ax2.set_ylabel(r'blood insulin level [unit/L]')
+ax2.set_ylabel(r'I(t) blood insulin level [unit/L]')
 ax2.set_xlim([0,24])
-ax2.set_ylim([-0.01,0.06])
+#ax2.set_ylim([-0.01,0.06])
 
 plt.show()
 
@@ -61,39 +120,10 @@ fig2 = plt.figure()
 ax1 = fig2.add_subplot(111)
 ln1 = ax1.plot(x[0, :], x[1, :], 'C0', label = "blood glucose level [mg/dL]") 
 
-ax1.set_xlabel(r'blood insulin level [unit/L]')
-ax1.set_ylabel(r'blood glucose level [mg/dL]')
+ax1.set_xlabel(r'I(t) blood insulin level [unit/L]')
+ax1.set_ylabel(r'G(t) blood glucose level [mg/dL]')
 #ax1.set_xlim([-0.01,0.06])
 #ax1.set_ylim([-10,60])
 ax1.grid(True)
-plt.show()
-
-"""
-x = np.arange(-5, 50)
-y = np.arange(0.0, 0.06, 0.001)
-P = np.concatenate((x, y), axis = 0) * dt
-u = [-gamma*dt, 1-delta*dt]*[y,x]
-v = [1-alpha*dt, beta*dt]
-
-x, y = np.meshgrid(x, y)
-u, v = np.meshgrid(u, v)
- 
-fig, axes = plt.subplots(1, 2, figsize=(12, 4.8))
- 
-lim = 8
-for ax in axes:
-    ax.set_xlim(-lim, lim)
-    ax.set_ylim(-lim, lim)
- 
-    ax.set_xticks(np.arange(-lim, lim, 1))
-    ax.set_yticks(np.arange(-lim, lim, 1))
- 
-    ax.grid()
-    ax.set_aspect('equal')
- 
-C = np.sqrt(u * u + v * v)
-axes[0].quiver(x, y, u, v)
-axes[1].quiver(x, y, u, v, C, scale=100, cmap='Blues')
- 
 plt.show()
 """
